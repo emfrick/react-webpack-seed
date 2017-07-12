62e94f03a5f0fbdf3104dceb103d0ae1b6635c88
Eric Frick <efrick@iq-inc.com>
2017-07-12 11:01:25 -0400

    Add environment switches and clean before build

--- webpack.config.js
+++ webpack.config.js
@@ -3,65 +3,92 @@
  */
 const webpack = require('webpack');
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const PATHS = {
+    ROOT: __dirname,
+    BUILD: path.join(__dirname, 'build'),
+    DIST: path.join(__dirname, 'dist'),
+    SRC: path.join(__dirname, 'src')
+};
+const PRODUCTION_ENVIRONMENT = 'PRODUCTION';
+const DEVELOPMENT_ENVIRONMENT = 'DEVELOPMENT';
+const CLEAN_PATHS = [PATHS.BUILD, PATHS.DIST];
 
 /**
  * Webpack Configuration
  */
-const config = {
-    context: path.join(__dirname, 'src'),
-    entry: './index.js',
-    devtool: 'source-map',
-    output: {
-        path: __dirname,
-        filename: 'bundle.js',
-        publicPath: '/' // Allows static content to be hosted from this location
-                        // HTML can now refer to /assets/img for all images
-    },
-    module: {
-        rules: [
-            {
-                test: /\.sass$/,
-                exclude: /node_modules/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['es2015', 'react', 'stage-0']
-                        }                        
-                    }                    
-                ]
-            },
-            {
-                test: /\.(jpg|png|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 25000    // Inline files below 25kB
-                        }
-                    }
-                ]                
-            }
-        ]
-    },
-    devServer: {
-        contentBase: './',   // Allows static content to be hosted from this location
+const config = (isProduction) => {    
+
+    return {
+        context: PATHS.SRC,
+        entry: './index.js',
+        devtool: isProduction ? 'source-map' : 'eval',
+        output: {
+            path: isProduction ? PATHS.DIST : PATHS.BUILD,
+            filename: 'bundle.js',
+            publicPath: '/' // Allows static content to be hosted from this location
                             // HTML can now refer to /assets/img for all images
-        stats: 'minimal',
-        publicPath: '/'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.sass$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['es2015', 'react', 'stage-0']
+                            }                        
+                        }                    
+                    ]
+                },
+                {
+                    test: /\.(jpg|png|svg)$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 25000    // Inline files below 25kB
+                            }
+                        }
+                    ]                
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(CLEAN_PATHS, {
+                root: PATHS.ROOT,
+                verbose: !isProduction
+            })
+        ],
+        devServer: {
+            contentBase: './',   // Allows static content to be hosted from this location
+                                // HTML can now refer to /assets/img for all images
+            stats: 'minimal',
+            publicPath: '/'
+        }
     }
 }
 
 /**
  * Module Export
  */
-module.exports = config;
+module.exports = (env) => {
+
+    let isProduction = (env && env === PRODUCTION_ENVIRONMENT);
+
+    console.log("ENVIRONEMNT:", isProduction ? PRODUCTION_ENVIRONMENT : DEVELOPMENT_ENVIRONMENT);
+
+    return config(isProduction);
+}
